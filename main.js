7d47bd005cbc1aa4c533620b8871cab858aeaa32
Roy Fu <fu90@purdue.edu>
2014-08-25 19:26:22 -0500

    finished first iteration of landing page

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
 	update();
+
+
+// PROJECTS LINK HOVER ANIMATION
+	$('body').on('mouseenter', '#projectsLink', function() {
+		$('#projectsLink').animate({
+			opacity: 0.8
+		}, 200);
+	});
+	$('body').on('mouseleave', '#projectsLink', function() {
+		$('#projectsLink').animate({
+			opacity: 1
+		}, 200);
+	});
+
+// ABOUT LINK HOVER ANIMATION
+	$('body').on('mouseenter', '#aboutLink', function() {
+		$('#aboutLink').animate({
+			opacity: 0.8
+		}, 200);
+	});
+	$('body').on('mouseleave', '#aboutLink', function() {
+		$('#aboutLink').animate({
+			opacity: 1
+		}, 200);
+	});
+
+// 
+	$('body').on('click', '#projectsLink', function() {
+		page = 1;
+		update();
+	});
+	$('body').on('click', '#aboutLink', function() {
+		page = 2;
+		update();
+	});
 });
 
 var html;
@@ -21,8 +56,9 @@ function updateHtml() {
 	switch(page) {
 		case landing:
 			html += '<div id="landing">';
-			html += 	'<h1>Velocicoder Studios</h1>';
 			html +=		'<img src="assets/Logo.svg">';
+			html += 	'<h1><span>V</span>elocicoder Studios</h1>';
+			html += 	'<h3><span id="projectsLink">Projects</span> | <span id="aboutLink">About</span></h3>';
 			html += '</div>';
 			break;
 		case projects:
